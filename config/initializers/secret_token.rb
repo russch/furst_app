3146208d008a5b1caf7870e8b508e6c4fe5473ac
russch <russch@gmail.com>
2013-07-02 09:21:22 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FurstApp::Application.config.secret_token = '0ac5cc0a469f7e8b1a70ff4ccc531baac0e018162f2257ec9e40224c750280d4f5f1caca77f2cc620da3a335b8a022c6926b0e4232e753d26b08e2b85035e702'
